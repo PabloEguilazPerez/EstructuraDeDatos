752fd5e509c36c12a09776fe94e06ff37cd74403
Pablo Eguilaz Pérez <pabloeguilazperez@gmail.com>
2025-10-09 13:07:53 +0200

    Update

---
+++ sesion-04/src/estdatos/SList.java
@@ -0,0 +1,37 @@
+package estdatos;
+
+import java.util.Iterator;
+
+public class SList<E> implements IList<E> {
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void add(int index, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
