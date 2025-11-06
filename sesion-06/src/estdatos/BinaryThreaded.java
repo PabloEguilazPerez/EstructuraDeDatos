26af382f1b4e0f358ac8394baf5da3136a34d133
Pablo Eguilaz Pérez <UO308329@uniovi.es>
2025-11-06 18:51:43 +0100

    update

--- sesion-06/src/estdatos/BinaryThreaded.java
+++ sesion-06/src/estdatos/BinaryThreaded.java
@@ -1,113 +1,125 @@
 package estdatos;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
-/**
- * Arbol binario con hilos inorden (Threaded Binary Tree)
- */
 public class BinaryThreaded<E> extends BinaryTreeImp<E> implements Iterable<E> {
 
-	private boolean isRightThread;
-	private boolean isLeftThread;
-	
-	private static final BinaryThreaded<?> EMPTY = new BinaryThreaded<>();
-	
-	public BinaryThreaded() {
-		super();
-	}
-	
-	public BinaryThreaded(E e) {
-		super(e);
-	}
-	
-	public BinaryThreaded(E e, BinaryTree<E> leftSubtree, BinaryTree<E> rightSubtree) {
-		super(e);
-	}
-	
-	public BinaryThreaded(BinaryTree<E> bt) {
-		super(bt);
-	}
-	
-	private static <T> BinaryThreaded<T> empty() {
-	 return (BinaryThreaded<T>) EMPTY;
-	}
-	
-	public BinaryTree<E> left() {
-		return isLeftThread ? empty() : left;
-	}
-	
-	public BinaryTree<E> right() {
-		return isLeftThread ? empty() : right;
-	}
+    private boolean isRightThread;
+    private boolean isLeftThread;
 
-	/**
-	 * For a given node, finds the right-most node in subtree and threads it back to
-	 * node
-	 * 
-	 * @param subtree for which we will find the right-most node
-	 * @param node    to be threaded
-	 */
-	private void setRightmostThread(BinaryThreaded<E> subtree, BinaryThreaded<E> node) {
-		BinaryThreaded<E> rightmost = subtree;
-		while (!rightmost.isRightThread && !rightmost.right().isEmpty()) {
-			rightmost = (BinaryThreaded<E>) rightmost.right;
-		}
-		rightmost.right = node;
-		rightmost.isRightThread = true;
-	}
+    private static final BinaryThreaded<?> EMPTY = new BinaryThreaded<>();
 
-	/**
-	 * For a given node, finds the left-most node in subtree and threads it back to
-	 * node
-	 * 
-	 * @param subtree for which we will find the left-most node
-	 * @param node    to be threaded
-	 */
-	private void setLeftmostThread(BinaryThreaded<E> subtree, BinaryThreaded<E> node) {
-		BinaryThreaded<E> leftmost = subtree;
-		while (!leftmost.isLeftThread && !leftmost.left().isEmpty()) {
-			leftmost = (BinaryThreaded<E>) leftmost.left;
-		}
-		leftmost.left = node;
-		leftmost.isLeftThread = true;
-	}
+    public BinaryThreaded() {
+        this.label = null;
+        this.left = null;
+        this.right = null;
+        this.isLeftThread = false;
+        this.isRightThread = false;
+    }
 
-	/**
-	 * Returns the string representation of the nodes of the tree, traversed in
-	 * inorder using threads and separated by a whitespace
-	 * 
-	 * @return the string representation of the tree
-	 */
-	public String inorder() {
-		StringBuilder sb = new StringBuilder();
-		BinaryThreaded<E> current = slideLeft();
-		while (!current.isEmpty()) {
-			sb.append(current.label + " ");
-			if (current.isRightThread)
-				current = (BinaryThreaded<E>) current.right;
-			else {
-				current = (BinaryThreaded<E>) current.right;
-				if (!current.isEmpty())
-					current = current.slideLeft();
-			}
-		}
-		return sb.toString();
-	}
+    public BinaryThreaded(E e) {
+        this(e, empty(), empty());
+    }
 
-	private BinaryThreaded<E> slideLeft() {
-		BinaryThreaded<E> current = this;
-		while (!current.left().isEmpty() && !current.isLeftThread) {
-			current = (BinaryThreaded<E>) current.left;
-		}
-		return current;
-	}
+    public BinaryThreaded(E e, BinaryTree<E> leftSubtree, BinaryTree<E> rightSubtree) {
+        if (e == null) {
+        	throw new NullPointerException();
+        }
+        
+        this.label = e;
 
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        if (leftSubtree == null || leftSubtree.isEmpty()) {
+            this.left = empty();
+            this.isLeftThread = true;
+        } else {
+            this.left = new BinaryThreaded<>(leftSubtree);
+            setRightmostThread((BinaryThreaded<E>) this.left, this);
+        }
+
+        if (rightSubtree == null || rightSubtree.isEmpty()) {
+            this.right = empty();
+            this.isRightThread = true;
+        } else {
+            this.right = new BinaryThreaded<>(rightSubtree);
+            setLeftmostThread((BinaryThreaded<E>) this.right, this);
+        }
+    }
+
+    public BinaryThreaded(BinaryTree<E> bt) {
+        this(bt.label(), bt.left(), bt.right());
+    }
+
+
+    private static <T> BinaryThreaded<T> empty() {
+        return (BinaryThreaded<T>) EMPTY;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this == EMPTY;
+    }
+
+    @Override
+    public E label() {
+        return isEmpty() ? null : label;
+    }
+
+    @Override
+    public BinaryTree<E> left() {
+        return isEmpty() ? null : (isLeftThread ? empty() : left);
+    }
+
+    @Override
+    public BinaryTree<E> right() {
+        return isEmpty() ? null : (isRightThread ? empty() : right);
+    }
+
+    private void setRightmostThread(BinaryThreaded<E> subtree, BinaryThreaded<E> node) {
+        BinaryThreaded<E> rightmost = subtree;
+        while (!rightmost.isRightThread && !rightmost.right().isEmpty()) {
+            rightmost = (BinaryThreaded<E>) rightmost.right;
+        }
+        rightmost.right = node;
+        rightmost.isRightThread = true;
+    }
+
+    private void setLeftmostThread(BinaryThreaded<E> subtree, BinaryThreaded<E> node) {
+        BinaryThreaded<E> leftmost = subtree;
+        while (!leftmost.isLeftThread && !leftmost.left().isEmpty()) {
+            leftmost = (BinaryThreaded<E>) leftmost.left;
+        }
+        leftmost.left = node;
+        leftmost.isLeftThread = true;
+    }
+
+    private BinaryThreaded<E> slideLeft() {
+        BinaryThreaded<E> current = this;
+        while (!current.left().isEmpty() && !current.isLeftThread) {
+            current = (BinaryThreaded<E>) current.left;
+        }
+        return current;
+    }
+
+    public String inorder() {
+        StringBuilder sb = new StringBuilder();
+        BinaryThreaded<E> current = slideLeft();
+        while (!current.isEmpty()) {
+            sb.append(current.label).append(" ");
+            if (current.isRightThread) {
+                current = (BinaryThreaded<E>) current.right;
+            } else {
+                current = (BinaryThreaded<E>) current.right;
+                if (!current.isEmpty()) {
+                    current = current.slideLeft();
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 }
